79225775445fb4ed5e3cfbee005d0eaaed42cb60
viniciusban <viniciusban@gmail.com>
2014-03-30 16:40:01 -0300

    Created repo

---
+++ README.md
@@ -0,0 +1,68 @@
+
+Migrate Bitbucket issues to Github
+==================================
+
+Migrate Bitbucket issues to Github, with comments.
+
+
+Installation
+------------
+
+1. Create a virtualenv and activate it.
+1. Clone this repo.
+1. `$ pip install -r requirements.txt`
+1. `$ python bb2gh_issues.py -h`
+
+You'll see an explanation of options.
+
+
+Example
+-------
+
+```
+$ python bb2gh_issues.py -l viniciusban -p my_password -r test_repo bb-issues.json
+```
+
+
+Notes and known limitations
+---------------------------
+
+You must [export your issues in Bitbucket](https://confluence.atlassian.com/display/BITBUCKET/Export+or+Import+Issue+Data), first.
+
+Issues import cannot be undone. So, I suggest you to run this process in a test repository before importing them to your real one.
+
+Images in our issues will not be migrated. If you can help me with this, I'll accept your pull request.
+
+All issues created on Github will have your `--login` user as the creator and the commenter. So, each issue and comment will have a note to tell you who and when created the issue (or comment).
+
+You can preserve issues' assignees. Edit `bb2gh_issues.py` and change the `ASSIGNEES` dict to represent your situation. Read comments on source code.
+
+Depending on how many issues and comments you have, this process can take a while. Don't panic.
+
+
+Options explained
+-----------------
+
+1. `-l`, `--login`
+
+    You need to be logged in to create issues in Github. This is the
+    username will be the creator of all issues and comments in this process.
+
+1. `-p`, `--password`
+
+    Password for `--login` user.
+
+1. `-r`, `--repo`
+
+    The repository will receive your issues.
+
+1. `-u`, `--user`
+
+    The username who owns the repo. Also known as the username that appears
+    in the repo url. I.e.: http://github.com/**this_is_the_username**/reponame
+
+    If ommited, `--login` will be used.
+
+1. `file`
+
+    The JSON file name with Bitbucket exported issues.
---
+++ bb2gh_issues.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals
+import argparse
+import json
+import sys
+from time import sleep
+from pygithub3 import Github
+
+# Assignees must have push access to repo. Otherwise, the process
+# will be interrupted.
+#
+# The ".DEFAULT" assignee is used when the assignee in Bitbucket no
+# longer exists in Github. I.e, for inactive commiters. Default commiter
+# can be None.
+#
+# The structure of this dict is:
+# ASSIGNEES = {'bitbucket_user': 'github_user'}
+
+ASSIGNEES = {'.DEFAULT': 'default_commiter',
+             'bitbucket_user': 'github_user',
+             None: None,  # no, this isn't wrong!
+             }
+
+
+def _parse_args():
+    """Create and parse command line args."""
+
+    argparser = argparse.ArgumentParser(
+        description='Import BitBucket issues into Github.')
+    argparser.add_argument(
+        'file', help='JSON file with BitBucket exported data')
+    argparser.add_argument(
+        '-l', '--login', required=True,
+        help='Github login (required)')
+    argparser.add_argument(
+        '-p', '--password', required=True,
+        help='Github password (required)')
+    argparser.add_argument(
+        '-r', '--repo', required=True,
+        help='Github repo where issues will imported (required)')
+    argparser.add_argument(
+        '-u', '--user', dest='username',
+        help=('Github user who repo belongs to. '
+              'If ommited, --login used.'))
+
+    argv = argparser.parse_args()
+    if not argv.username:
+        argv.username = argv.login
+    return argv
+
+
+def import_issue(bitbucket_data, argv):
+    """Import a single issue."""
+
+    comment_message = []
+
+    # print("=" * 50)
+    # print("Issue {id}".format(**bitbucket_data))
+    # print("Bitbucket data")
+    # print("--------------")
+    # print(bitbucket_data)
+    print('\n')
+    print('* Importing issue {id}'.format(**bitbucket_data))
+
+    labels = ['imported',  # you'll know what issues were imported.
+              bitbucket_data['kind'],
+              bitbucket_data['priority']
+              ]
+
+    if 'hold' in bitbucket_data['status']:
+        labels.append(bitbucket_data['status'])
+
+    if bitbucket_data['assignee'] in ASSIGNEES:
+        assignee = ASSIGNEES[bitbucket_data['assignee']]
+    else:
+        assignee = ASSIGNEES['.DEFAULT']
+        comment_message.append(
+            '(Original issue was assigned to {assignee})'
+            ''.format(**bitbucket_data))
+
+    issue_body = '\n\n'.join([
+        '(Original issue {id} created by {reporter} on {created_on})',
+        '{content}']
+    ).format(**bitbucket_data)
+
+    github_data = {
+        'title': bitbucket_data['title'],
+        'body': issue_body,
+        'labels': labels,
+        'assignee': assignee,
+    }
+
+    # print("Github data")
+    # print("-----------")
+    # print(github_data)
+
+    github_issue = argv.gh.issues.create(
+        github_data, user=argv.username, repo=argv.repo)
+
+    print('Imported as {github_issue}'.format(github_issue=github_issue))
+
+    if bitbucket_data['status'] == 'resolved':
+        comment_message.append(
+            '(Original issue {id} closed on {updated_on})'
+            ''.format(**bitbucket_data))
+
+        github_data['state'] = 'closed'
+        github_updated_issue = argv.gh.issues.update(
+            github_issue.number, github_data,
+            user=argv.username, repo=argv.repo)
+        print('Closed'.format(github_issue=github_issue))
+
+    if comment_message:
+        argv.gh.issues.comments.create(
+            number=github_issue.number,
+            message='\n\n'.join(comment_message),
+            user=argv.username, repo=argv.repo)
+
+    return github_issue.number
+
+
+def import_comment(github_issue_number, comment, argv):
+    """Import a single comment."""
+
+    if not comment['content']:
+        # ignores status changing comments.
+        return
+
+    ret = argv.gh.issues.comments.create(
+        number=github_issue_number,
+        message=('(Original comment by {user} on {created_on})\n\n'
+                 '{content}\n\n'
+                 ''.format(**comment)),
+        user = argv.username,
+        repo = argv.repo)
+
+
+def import_issues_and_comments(issues, comments, argv):
+    issues_read = issues_imported = 0
+    comments_read = comments_imported = 0
+
+    issues = sorted(issues, key=lambda x: x['id'])
+
+    for bitbucket_issue in issues:
+        issues_read += 1
+
+        github_issue_number = import_issue(bitbucket_issue, argv=argv)
+        issues_imported += 1
+
+        # get comments for this issue
+        issue_comments = [c for c in comments if c['issue'] ==
+                          bitbucket_issue['id']]
+        # sort to insert comments in creation order.
+        issue_comments = sorted(issue_comments,
+                                key=lambda x: x['id'])
+
+        for bitbucket_comment in issue_comments:
+            comments_read += 1
+             # delay comment insertion to order comments properly.
+            sleep(1)
+            import_comment(
+                github_issue_number=github_issue_number,
+                comment=bitbucket_comment,
+                argv=argv)
+            comments_imported += 1
+
+    return {
+        'issues_read': issues_read,
+        'issues_imported': issues_imported,
+        'comments_read': comments_read,
+        'comments_imported': comments_imported
+    }
+
+
+def main():
+
+    argv = _parse_args()
+
+    with open(argv.file) as f:
+        bitbucket_data = json.load(f)
+
+    gh = Github(login=argv.login, password=argv.password)
+    argv.gh = gh
+
+    ret = import_issues_and_comments(
+        issues=bitbucket_data['issues'][:],
+        comments=bitbucket_data['comments'],
+        argv=argv)
+
+    print("""
+Done.
+
+Import overview
+===============
+
+Issues:
+    read: {issues_read}
+    imported: {issues_imported}
+Comments:
+    read: {comments_read}
+    imported: {comments_imported}
+""".format(**ret)
+          )
+
+
+if __name__ == '__main__':
+    main()
